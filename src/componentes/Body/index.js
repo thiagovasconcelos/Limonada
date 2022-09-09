d90977442cdc31aede6bc2c3c86dcd3917b7302a
Aluno <aluno@naverecife.local>
2022-09-08 21:31:18 -0300

    add body

--- src/componentes/Body/index.js
+++ src/componentes/Body/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Fhome from "../../assets/fotoHome.jpg"
-
+import Fh2 from "../../assets/Fh2.jpg"
+import "./style.css"
 export default function Body(){
     return(
-
-        <img classeName="Fhome" src={Fhome} alt="FotoHome" ></img>
+        <div className="container">
+           <div className="Fh2">
+                <img   src={Fh2} alt="FotoHome2" width="100%" height="100%" ></img>
+            </div>
+            <div className="container-img">
+                <img classeName="Fhome" src={Fhome} alt="FotoHome" width="100%" ></img>
+            </div>
+        </div>
+        
     );
 }
